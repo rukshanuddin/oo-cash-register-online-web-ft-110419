1570dcc26b60e95528f4a361dd3f0da50385dad5
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-13 17:54:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ class CashRegister
   def apply_discount
     #if @discount == 20
       @total = (@total * (100 - discount) *0.01).to_int
-      return "After the discount, the total comes to $#{@total}." 
+      return "After the discount, the total comes to $#{@total}."
     #else
     #  return "There is no discount to apply."
     #end
